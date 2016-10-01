86a3cd52467b3099faa101335f3aa879bea21f8e
Raphael Benitte <benitte@ekino.com>
2016-10-01 19:30:18 +0900

    doc(readme): update README

--- misc/readmeData.js
+++ misc/readmeData.js
@@ -1,12 +1,18 @@
 const _       = require('lodash')
 const mapping = require('../mapping')
+const samples = require('../samples')
 
 const data = {
     endpoints: [],
+    samples:   [],
 }
 
 _.forOwn(mapping, (config, type) => {
     data.endpoints.push(`/charts/${type}`)
+})
+
+_.forOwn(samples, (config, id) => {
+    data.samples.push(id)
 })
 
 
