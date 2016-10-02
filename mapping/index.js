498a5f18aa52c9ca63d061cfe2f11db58afd649b
Raphael Benitte <benitte@ekino.com>
2016-10-02 11:27:52 +0900

    chore(license): add license

--- mapping/index.js
+++ mapping/index.js
@@ -1,3 +1,13 @@
+/*
+ * This file is part of the nivo project.
+ *
+ * (c) 2016 Raphaël Benitte
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+'use strict'
+
 const bar    = require('./bar')
 const line   = require('./line')
 const bubble = require('./bubble')
